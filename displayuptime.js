78ed6bc3fbee085c3ebe377f8a6473bd1211b846
Aayush Bajirao <aayubaji1208@gmail.com>
2022-06-06 12:06:16 +0530

    First Commit

---
+++ displayuptime.js
@@ -0,0 +1,14 @@
+const os = require('os');
+
+var ut_sec = os.uptime();
+var ut_min = Math.floor(ut_sec/60);
+var ut_hour = Math.floor(ut_min/60);
+
+ut_sec = ut_sec % 60
+ut_min = ut_min % 60
+ut_hour = ut_hour % 60
+
+console.log('Up Time For Machine is')
+console.log('Hours', ut_hour);
+console.log('Minutes ', ut_min);
+console.log('Seconds ', ut_sec);
---
+++ second.js
@@ -0,0 +1,9 @@
+harry = {
+    name: "Harry",
+    favNum: 3,
+    developer: true, 
+  };
+  
+  
+  
+  module.exports = harry
